8734c0232fe100e259867956bdc9521b930cc981
Rob Lyon <nosignsoflifehere@gmail.com>
2013-02-26 12:08:59 -0800

    version bump to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "nosignsoflifehere@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures pretty-prompt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 %w{ redhat centos fedora }.each do |os|
   supports os
